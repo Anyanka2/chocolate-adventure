d41ff287674127070efc80424b529c4d571de2e4
Maryna <mlegosha@gmail.com>
2023-03-29 20:58:26 +0300

    video-done

--- src/scripts/how-its-made.js
+++ src/scripts/how-its-made.js
@@ -2,13 +2,13 @@
 const videoBtn = document.querySelector('.how-btn');	
 	videoBtn.addEventListener('click', playVideo);
 	function playVideo() {
-	let videoContainer = document.querySelector('.process-video');
+	let videoContainer = document.querySelector('.video-container');
 	let iframe = document.createElement('iframe');
 	iframe.setAttribute(
         'src',
         'https://www.youtube.com/embed/xPe1jMuX32s'
-	);
-	iframe.setAttribute('allowfullscreen', '');
+        );
+    iframe.setAttribute('allowfullscreen', '');
 	videoContainer.innerHTML = '';
 	videoContainer.appendChild(iframe);
     }
