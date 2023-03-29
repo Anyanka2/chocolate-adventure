35f7e759ed5c48a2e43183b05b7a6d92cda2cca3
Liu3oIT <liutyivova995@gmail.com>
2023-03-29 12:14:32 +0300

    update

--- src/scripts/modals-buy.js
+++ src/scripts/modals-buy.js
@@ -1,18 +1,30 @@
 (() => {
-    const refs = {
-      openModalBtns: document.querySelectorAll("[data-modal-open-buy]"),
-      closeModalBtn: document.querySelector("[data-modal-close-buy]"),
-      modal: document.querySelector("[data-moda-buy]"),
-    };
-    
-    refs.openModalBtns.forEach(addToggleEvent);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  
-    function addToggleEvent(element) {
-      element.addEventListener("click", toggleModal);
-    }
-  })();
+  const refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-open-buy]"),
+    closeModalBtn: document.querySelector("[data-modal-close-buy]"),
+    submitBtn: document.querySelector("[hero-button-buy-submit]"),
+    modal: document.querySelector("[data-moda-buy]"),
+    postModal: document.querySelector("[top-sellers-thankyou]")
+  };
+
+  refs.openModalBtns.forEach(addToggleEvent);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.submitBtn.addEventListener("click", close);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function close(){
+    toggleModal();
+    togglePostModal();
+  }
+
+  function togglePostModal() {
+    refs.postModal.classList.toggle("is-hidden");
+  }
+
+  function addToggleEvent(element) {
+    element.addEventListener("click", toggleModal);
+  }
+})();
